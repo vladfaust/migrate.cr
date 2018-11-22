2b32e1420faabb6c3b5777b13ba8e3e3e3c65be7
Vlad Faust <mail@vladfaust.com>
2018-11-22 13:31:21 +0300

    Add migrator spec

--- spec/migrations/10_create_baz.sql
+++ spec/migrations/10_create_baz.sql
@@ -7,5 +7,17 @@ CREATE TABLE baz (
 -- Indexes
 CREATE UNIQUE INDEX baz_content_index ON baz (content);
 
+-- Statements that might contain semicolons
+-- +migrate start
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS
+$$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+-- +migrate end;
+
 -- +migrate down
 DROP TABLE baz;
